aac80d67988f1d51440f1f3cb2ee9801437b54cc
jannis <jannis@tadris.de>
2019-08-24 21:13:17 +0200

    #20 Back-Button in Settings screen crashes on earlier Android versions

--- app/src/main/java/de/tadris/fitness/activity/SettingsActivity.java
+++ app/src/main/java/de/tadris/fitness/activity/SettingsActivity.java
@@ -325,9 +325,7 @@ public class SettingsActivity extends PreferenceActivity {
     public boolean onMenuItemSelected(int featureId, MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-            if (!super.onMenuItemSelected(featureId, item)) {
-                NavUtils.navigateUpFromSameTask(this);
-            }
+            finish();
             return true;
         }
         return super.onMenuItemSelected(featureId, item);
